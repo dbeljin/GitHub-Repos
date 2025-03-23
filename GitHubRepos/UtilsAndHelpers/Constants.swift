b9d63f78e85122dcb4c6d9498e7c09c6118bc24e
Dusan Beljin <dbeljin@gmail.com>
2025-03-23 14:46:00 +0100

    Minor change for easier readability

--- GitHubRepos/UtilsAndHelpers/Constants.swift
+++ GitHubRepos/UtilsAndHelpers/Constants.swift
@@ -10,10 +10,11 @@ import Foundation
 struct Constants {
     //TODO: Improvement for hardCodedUser - In some future phases create screen where user will firstly enter username
     public static let hardCodedUser: String = "octocat"
+    public static let timeoutInterval: TimeInterval = 20
+    
     private static let baseUrlProduction = "https://api.github.com/"
     public static let userReposEndpoint: String = "users/%@/repos"
     public static let repoTagsEndpoint: String = "repos/%@/%@/tags"
-    public static let timeoutInterval: TimeInterval = 20
     
     //If in future we have development/demo/testing environments
     //we can just adopt here base urls based on environment and all api calls will continue working
